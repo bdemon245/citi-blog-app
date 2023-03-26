3231a94440e8fd44a07e01b580e02b1917c7ef48
Mojahidul Islam <bdemon245@gmail.com>
2023-03-26 17:42:18 +0600

    tests

--- resources/views/layouts/backendapp.blade.php
+++ resources/views/layouts/backendapp.blade.php
@@ -85,7 +85,7 @@ License: You must have a valid license purchased only from themeforest(the above
                         <ul class="dropdown-content bg-theme-26 dark-bg-dark-6 text-white">
                             <li class="p-2">
                                 <div class="fw-medium text-white">{{ str(auth()->user()->name)->headline() }}</div>
-                                <div class="fs-xs text-theme-28 mt-0.5 dark-text-gray-600">{{auth()->user()->roles->first()->name}}</div>
+                                <div class="fs-xs text-theme-28 mt-0.5 dark-text-gray-600">{{dd(auth()->user()->roles)}}</div>
                             </li>
                             <li>
                                 <hr class="dropdown-divider border-theme-27 dark-border-dark-3">
